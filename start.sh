c3cb172ba1c55a5a1e79d750be5ca083ded72cee
Sven Wagener <sven@awesome.ug>
2017-04-22 14:02:26 +0200

    First working xdebug config

--- start.sh
+++ start.sh
@@ -12,6 +12,7 @@ docker exec ${dirname}_wordpress_1 wp core language update --allow-root
 docker exec ${dirname}_wordpress_1 wp core install --url=localhost --title=Shipcloud --admin_user=admin --admin_password=admin --admin_email=info@example.com --allow-root
 docker exec ${dirname}_wordpress_1 wp plugin install woocommerce --allow-root
 docker exec ${dirname}_wordpress_1 wp plugin update woocommerce --allow-root
+docker exec ${dirname}_wordpress_1 wp theme update twentyseventeen twentysixteen twentyfifteen --allow-root
 
 if [ ! -d ./src/plugins/woocommerce-shipcloud ]; then
     git clone git@github.com:awsmug/woocommerce-shipcloud.git ./src/plugins/woocommerce-shipcloud
